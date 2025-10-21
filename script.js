b1c321dcf1df2d222043797fb60347165537ea8e
glebgemin-dotcom <glebgemin@gmail.com>
2025-10-21 19:42:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let burger=document.querySelector(".burger");
+let menu= document.querySelector("ul");
 burger.addEventListener("click",start)
 function start(){
     burger.classList.toggle("active")
+    menu.classList.toggle("show")
 }
